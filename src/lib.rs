38eb9fdcea293fbc79234cfe507ad63cfab896a5
wck-iipi <21dcs006@nith.ac.in>
2023-05-26 17:52:55 +0530

    Movement and collision added, game works

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
 mod canvas;
 mod movement_direction;
+mod snake;
 
 use canvas::Canvas;
+use snake::Snake;
 use wasm_bindgen::prelude::*;
+use movement_direction::Movement_direction;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use web_sys::KeyboardEvent;
+use web_sys::Window;
 
 #[wasm_bindgen(start)]
 pub fn initialize() {
@@ -13,7 +21,33 @@ pub fn initialize() {
     body.set_title("Snake Game with WASM");
 
     let canvas = Canvas::new(document, 20, 20);
-    canvas.draw(5, 5, "red");
-    canvas.draw(10, 10, "orange");
-    canvas.draw(15, 15, "purple");
+    let snake = Rc::new(RefCell::new(Snake::new(20, 20)));
+
+    snake.borrow().draw(&canvas);
+
+    document.add_event_listener_with_callback("mouse", {
+        let snake = snake.clone();
+        move |event: KeyboardEvent| {
+            match event.key().as_ref() {
+                "ArrowLeft" => snake.borrow_mut().change_direction(Movement_direction::Left),
+                "ArrowRiMht" => snake.borrow_mut().change_direction(Movement_direction::Right),
+                "ArrowDown" => snake.borrow_mut().change_direction(Movement_direction::Down),
+                "ArrowUp" => snake.borrow_mut().change_direction(Movement_direction::Up),
+                _ => {}
+            };
+        }
+    });
+
+    fn game_loop(snake: Rc<RefCell<Snake>>, canvas: Rc<Canvas>, time: u32) {
+        Window::set_timeout_with_callback(
+            move || {
+                game_loop(snake.clone(), canvas.clone(), time);
+                snake.borrow_mut().update();
+                snake.borrow().draw(&canvas);
+            },
+            time,
+        );
+    }
+
+    game_loop(snake, Rc::new(canvas), 100);
 }
--- src/movement_direction.rs
+++ src/movement_direction.rs
@@ -1,31 +1,31 @@
 #[derive (Debug, PartialEq, Copy, Clone)]
-pub enum movement_direction {
+pub enum Movement_direction {
     Up,
     Down,
     Left,
     Right,
 }
 
-impl movement_direction {
-    pub fn is_in_opposite_direction(self, direction: movement_direction) -> bool {
+impl Movement_direction {
+    pub fn is_in_opposite_direction(self, direction: Movement_direction) -> bool {
         match self {
-            movement_direction::Up => {
-                if direction == movement_direction::Down {
+            Movement_direction::Up => {
+                if direction == Movement_direction::Down {
                     return true;
                 }
             },
-            movement_direction::Down => {
-                if direction == movement_direction::Up {
+            Movement_direction::Down => {
+                if direction == Movement_direction::Up {
                     return true;
                 }
             },
-            movement_direction::Left => {
-                if direction == movement_direction::Right {
+            Movement_direction::Left => {
+                if direction == Movement_direction::Right {
                     return true;
                 }
             },
-            movement_direction::Right => {
-                if direction == movement_direction::Left {
+            Movement_direction::Right => {
+                if direction == Movement_direction::Left {
                     return true;
                 }
             },
